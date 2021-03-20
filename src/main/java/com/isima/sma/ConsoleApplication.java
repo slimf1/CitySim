7d870491e334d549641f5167b63b7f8ad7fc3ff3
slimf1 <slimane.f41@gmail.com>
2021-03-20 20:39:36 +0100

    Add Mersenne Twister RNG

--- src/main/java/com/isima/sma/ConsoleApplication.java
+++ src/main/java/com/isima/sma/ConsoleApplication.java
@@ -1,10 +1,12 @@
 package com.isima.sma;
 
 import com.isima.sma.ui.ConsoleUserInterface;
+import com.isima.sma.utils.MTRandom;
 
 public class ConsoleApplication {
     public static void main(String[] args) {
         ConsoleUserInterface ui = new ConsoleUserInterface();
+        MTRandom.getInstance().setSeed(1);
         ui.start();
     }
 }
--- src/main/java/com/isima/sma/JavaFxApplication.java
+++ src/main/java/com/isima/sma/JavaFxApplication.java
@@ -1,11 +1,12 @@
 package com.isima.sma;
 
 import com.isima.sma.ui.FxUserInterface;
+import com.isima.sma.utils.MTRandom;
 import javafx.application.Application;
 
 public class JavaFxApplication {
     public static void main(String[] args) {
-        // Params etc
         Application.launch(FxUserInterface.class, args);
+        MTRandom.getInstance().setSeed(1);
     }
 }
