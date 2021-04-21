0657b94ca87496466dfceccf9c5266d063435492
slimf1 <slimane.f41@gmail.com>
2021-04-21 21:23:03 +0200

    Set time limits on enum

--- src/main/java/com/isima/sma/time/TimeOfDay.java
+++ src/main/java/com/isima/sma/time/TimeOfDay.java
@@ -1,10 +1,26 @@
 package com.isima.sma.time;
 
-public enum TimeOfDay{
-    DAWN,
-    MORNING,
-    MIDDAY,
-    AFTERNOON,
-    DUSK,
-    NIGHT
+public enum TimeOfDay {
+    DAWN(4, 7),
+    MORNING(7, 12),
+    MIDDAY(12, 14),
+    AFTERNOON(14, 18),
+    DUSK(18, 21),
+    NIGHT(21, 4);
+
+    private int lower;
+    private int upper;
+
+    TimeOfDay(int lower, int upper) {
+        this.lower = lower;
+        this.upper = upper;
+    }
+
+    public int getLower() {
+        return lower;
+    }
+
+    public int getUpper() {
+        return upper;
+    }
 }
